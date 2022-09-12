aa34dcfd00f8cae8d4b9cdaabd541e10abcfccf8
akostiucenko <kostiuchenko@soramitsu.co.jp>
2022-09-12 11:42:27 +0500

    improvements

--- wasm-triggers-migration/src/main/kotlin/jp/co/soramitsu/Main.kt
+++ wasm-triggers-migration/src/main/kotlin/jp/co/soramitsu/Main.kt
@@ -2,15 +2,12 @@ package jp.co.soramitsu
 
 import jp.co.soramitsu.iroha2.asDomainId
 import jp.co.soramitsu.iroha2.asName
-import jp.co.soramitsu.iroha2.asValue
 import jp.co.soramitsu.iroha2.client.Iroha2Client
 import jp.co.soramitsu.iroha2.generated.datamodel.account.AccountId
-import jp.co.soramitsu.iroha2.generated.datamodel.trigger.Trigger
 import jp.co.soramitsu.iroha2.generated.datamodel.trigger.TriggerId
 import jp.co.soramitsu.iroha2.keyPairFromHex
 import jp.co.soramitsu.iroha2.query.QueryBuilder
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
 import java.io.File
 import java.net.URL
 import java.security.KeyPair
@@ -25,7 +22,9 @@ fun main(vararg args: String): Unit = runBlocking {
         args[1], // directory with WASM contracts
         AccountId(args[2].asName(), args[3].asDomainId()),
         keyPairFromHex(args[4], args[5])
-    ).also { results ->
+    ).map { triggerId ->
+        triggerId.name.string
+    }.also { results ->
         println("$results have been updated")
     }
 }
@@ -34,7 +33,7 @@ private suspend fun Iroha2Client.update(
     directory: String,
     admin: AccountId,
     keyPair: KeyPair
-): List<String> {
+): List<TriggerId> {
     val wasmFiles = File(directory).walk()
         .filter { !it.isDirectory }
         .filter { it.name.endsWith(".wasm") }
@@ -50,55 +49,28 @@ private suspend fun Iroha2Client.update(
         throw RuntimeException("Found triggers: $idsValues, provided files: $wasmFileNames")
     }
 
-    wasmFiles.forEach { file ->
-        val triggerId = TriggerId(file.key.removeSuffix(".wasm").asName())
-        val trigger = QueryBuilder.findTriggerById(triggerId)
-            .account(admin)
-            .buildSigned(keyPair)
-            .let { this.sendQuery(it) }
+    return wasmFiles.map { file ->
+        triggerIds.filter { id ->
+            id.name.string.startsWith(file.key.removeSuffix(".wasm"))
+        }.map { id ->
+            val trigger = QueryBuilder.findTriggerById(id)
+                .account(admin)
+                .buildSigned(keyPair)
+                .let { this.sendQuery(it) }
 
-        this.unregisterTrigger(admin, keyPair, triggerId)
-        this.registerTrigger(admin, keyPair, trigger, file.value.readBytes())
-    }
-
-    return wasmFileNames
-}
-
-private suspend fun Iroha2Client.unregisterTrigger(
-    admin: AccountId,
-    keyPair: KeyPair,
-    triggerId: TriggerId,
-    timeout: Long = 30000
-) = this.sendTransaction {
-    account(admin)
-    unregisterTrigger(triggerId.name.string)
-    buildSigned(keyPair)
-}.also {
-    withTimeout(timeout) {
-        it.await()
-    }
-}
-
-private suspend fun Iroha2Client.registerTrigger(
-    admin: AccountId,
-    keyPair: KeyPair,
-    trigger: Trigger<*>,
-    wasm: ByteArray,
-    timeout: Long = 30000
-) = this.sendTransaction {
-    account(admin)
-    registerWasmTrigger(
-        trigger.id,
-        wasm,
-        trigger.action.repeats,
-        admin,
-        trigger.action.metadata,
-        trigger.action.filter
-    )
-    setKeyValue(admin, trigger.id.name, trigger.id.name.string.asValue())
-    buildSigned(keyPair)
-}.also {
-    withTimeout(timeout) {
-        it.await()
-    }
+            this.sendTransaction {
+                account(admin)
+                unregisterTrigger(id.name.string)
+                registerWasmTrigger(
+                    trigger.id,
+                    file.value.readBytes(),
+                    trigger.action.repeats,
+                    admin,
+                    trigger.action.metadata,
+                    trigger.action.filter
+                )
+                buildSigned(keyPair)
+            }.let { id }
+        }
+    }.flatten()
 }
