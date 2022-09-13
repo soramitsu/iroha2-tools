bfe2dd6ce3d8d6e66ffa82dbd3b136edb6fb4c9e
akostiucenko <kostiuchenko@soramitsu.co.jp>
2022-09-13 18:45:38 +0500

    remove unnecessary dependencies

--- wasm-triggers-migration/build.gradle.kts
+++ wasm-triggers-migration/build.gradle.kts
@@ -15,13 +15,8 @@ repositories {
 
 dependencies {
     val iroha2Ver by System.getProperties()
-    val kotlinVer by System.getProperties()
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation(kotlin("stdlib-jdk8"))
-
-    implementation("org.apache.commons:commons-csv:1.8")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.0")
@@ -30,15 +25,6 @@ dependencies {
     api("com.github.hyperledger.iroha-java:client:$iroha2Ver")
     implementation("com.github.hyperledger.iroha-java:model:$iroha2Ver")
     implementation("com.github.hyperledger.iroha-java:block:$iroha2Ver")
-
-    testImplementation("com.github.hyperledger.iroha-java:testcontainers:$iroha2Ver")
-    testImplementation("com.github.hyperledger.iroha-java:test-tools:$iroha2Ver")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlinVer")
-
-    // crypto
-    implementation("net.i2p.crypto:eddsa:0.3.0")
-    implementation("org.bouncycastle:bcprov-jdk15on:1.65")
-    implementation("com.github.multiformats:java-multihash:1.3.0")
 }
 
 val compileKotlin: KotlinCompile by tasks
