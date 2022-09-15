47c04b3abfa45b6ecac0ad88bcb5b4ef60ab463a
akostiucenko <kostiuchenko@soramitsu.co.jp>
2022-09-15 18:01:05 +0500

    trigger_mint_credit test

--- wasm-triggers-migration/src/test/kotlin/jp/co/soramitsu/Tests.kt
+++ wasm-triggers-migration/src/test/kotlin/jp/co/soramitsu/Tests.kt
@@ -4,10 +4,16 @@ import jp.co.soramitsu.iroha2.asName
 import jp.co.soramitsu.iroha2.client.Iroha2Client
 import jp.co.soramitsu.iroha2.engine.IrohaTest
 import jp.co.soramitsu.iroha2.engine.WithIroha
+import jp.co.soramitsu.iroha2.generateKeyPair
 import jp.co.soramitsu.iroha2.generated.Duration
+import jp.co.soramitsu.iroha2.generated.datamodel.account.AccountId
+import jp.co.soramitsu.iroha2.generated.datamodel.events.data.filters.IdFilterAccountId
+import jp.co.soramitsu.iroha2.generated.datamodel.events.data.filters.account.AccountEventFilter
 import jp.co.soramitsu.iroha2.generated.datamodel.metadata.Metadata
 import jp.co.soramitsu.iroha2.generated.datamodel.trigger.TriggerId
 import jp.co.soramitsu.iroha2.generated.datamodel.trigger.action.Repeats
+import jp.co.soramitsu.iroha2.toIrohaPublicKey
+import jp.co.soramitsu.iroha2.transaction.EntityFilters
 import jp.co.soramitsu.iroha2.transaction.EventFilters
 import jp.co.soramitsu.iroha2.transaction.Filters
 import kotlinx.coroutines.runBlocking
@@ -25,6 +31,9 @@ class Tests : IrohaTest<Iroha2Client>() {
     @WithIroha(DefaultGenesis::class)
     fun `should update all smart contracts`(): Unit = runBlocking {
         registerNftStatsTrigger()
+        registerMintCreditTrigger(
+            AccountId("Bob".asName(), DEFAULT_DOMAIN_ID)
+        )
 
         Helper(client).update(
             this.javaClass.classLoader
@@ -33,7 +42,38 @@ class Tests : IrohaTest<Iroha2Client>() {
             ALICE_ACCOUNT_ID,
             ALICE_KEYPAIR
         ).also {
-            assertEquals(1, it.size)
+            assertEquals(2, it.size)
+        }
+    }
+
+    private suspend fun registerMintCreditTrigger(newAccount: AccountId) {
+        client.sendTransaction {
+            account(ALICE_ACCOUNT_ID)
+            registerAccount(
+                newAccount,
+                listOf(generateKeyPair().public.toIrohaPublicKey()),
+                Metadata(mapOf())
+            )
+            registerWasmTrigger(
+                TriggerId("trigger_mint_credit".asName()),
+                this.javaClass.classLoader
+                    .getResource("trigger_mint_credit.wasm")
+                    .readBytes(),
+                Repeats.Indefinitely(),
+                ALICE_ACCOUNT_ID,
+                Metadata(mapOf()),
+                Filters.data(
+                    EntityFilters.byAccount(
+                        IdFilterAccountId(newAccount),
+                        AccountEventFilter.ByMetadataInserted()
+                    )
+                )
+            )
+            buildSigned(ALICE_KEYPAIR)
+        }.also {
+            withTimeout(30000) {
+                it.await()
+            }
         }
     }
 
