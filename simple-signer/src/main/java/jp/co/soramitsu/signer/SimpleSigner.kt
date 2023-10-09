46a1363dbb4679035dcba9336caa60e69792f106
Timur Guskov <guskov@soramitsu.co.jp>
2023-10-09 16:56:24 +0300

    feature: update Iroha2 version to rc19

--- simple-signer/src/main/java/jp/co/soramitsu/signer/SimpleSigner.kt
+++ simple-signer/src/main/java/jp/co/soramitsu/signer/SimpleSigner.kt
@@ -3,22 +3,23 @@ package jp.co.soramitsu.signer
 import jp.co.soramitsu.iroha2.keyPairFromHex
 import jp.co.soramitsu.iroha2.sign
 import jp.co.soramitsu.iroha2.toHex
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-class SimpleSigner {
+@SpringBootApplication
+class SimpleSigner
 
-    fun main(args: Array<String>) {
-        if (args.size != 3) {
-            println("Specify public_key, private_key and text to sign")
-            return
-        }
-        val publicKey = args[0]
-        val privateKey = args[1]
-        val toSign = args[2]
-
-        val keyPair = keyPairFromHex(publicKey, privateKey)
-
-        val signature = keyPair.private.sign(toSign.toByteArray(Charsets.UTF_8)).toHex()
-
-        println("Signed message (Hex): $signature")
+fun main(args: Array<String>) {
+    if (args.size != 3) {
+        println("Specify public_key, private_key and text to sign")
+        return
     }
+    val publicKey = args[0]
+    val privateKey = args[1]
+    val toSign = args[2]
+
+    val keyPair = keyPairFromHex(publicKey, privateKey)
+
+    val signature = keyPair.private.sign(toSign.toByteArray(Charsets.UTF_8)).toHex()
+
+    println("Signed message (Hex): $signature")
 }
